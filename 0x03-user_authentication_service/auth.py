503d399ea71abf9ffa38bbcaaee9c763377a7e00
Ahmed Rady <rady20991@gmail.com>
2024-11-21 22:09:06 +0200

    Credentials validation

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -25,6 +25,19 @@ class Auth:
         new_user = self._db.add_user(email, hashed_password)
         return new_user
 
+    def valid_login(self, email: str, password: str) -> bool:
+        """Credentials validation"""
+        try:
+            user = self._db.find_user_by(email=email)
+            if user:
+                encode_pass = password.encode('utf-8')
+                pass_bytes = user.hashed_password
+                return bcrypt.checkpw(encode_pass, pass_bytes)
+            else:
+                return False
+        except NoResultFound:
+            return False
+
 
 def _hash_password(password: str) -> bytes:
     """Return password as bytes"""
